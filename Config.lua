6a9a271c6eff2c1ee1b0c2246a967d9a32988aa5
Joe Development <146123038+Joe-Development@users.noreply.github.com>
2024-01-19 02:53:57 -0500

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.chat_time = {
+    admin = 'chat_time.admin',
+    bypass = 'chat_time.bypass',
+    discord = {
+        webhook = 'https://discord.com/api/webhooks/1197296932243111936/eC-UF9mKwJSUIwRgeaHUwwD75sSsnE7JYY-nrXWamptOZ2m0qF1PI2Eq9c0mxbYjerGC',
+        name = 'Chat Timeout - Development',
+        footer = 'Chat Timeout - Development'
+    }
+}
