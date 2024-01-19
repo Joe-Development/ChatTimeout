2d99a6b5ab4ee4eaa26c9d215e15c8652cff311c
Joe Development <146123038+Joe-Development@users.noreply.github.com>
2024-01-19 02:54:29 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'JoeV2'
+description 'A Chat Timeout Script for FiveM'
+version '1.0.0'
+
+
+client_scripts {
+    'client.lua'
+}
+
+
+
+server_scripts {
+    'Config.lua',
+    'server.lua'
+}
