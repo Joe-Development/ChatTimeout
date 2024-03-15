b1493cee9d14f285d14bb2595a049ebfe62438c1
Freech <144473764+freech-dev@users.noreply.github.com>
2024-03-15 13:13:33 +0000

    Removed the need for thread
    Removed threads completly and used os.time so when the script isnt being used it does not take any resources

--- server.lua
+++ server.lua
@@ -1,82 +1,6 @@
 local timeoutList = {}
 
-function TimeoutPlayer(playerId, timeoutMinutes, reason)
-    local timeoutSeconds = timeoutMinutes * 60
-    timeoutList[playerId] = { time = timeoutSeconds, reason = reason }
-end
-
-function IsPlayerTimedOut(playerId)
-    return timeoutList[playerId] and timeoutList[playerId].time > 0
-end
-
-function UpdateTimeouts()
-    for playerId, data in pairs(timeoutList) do
-        if data.time > 0 then
-            data.time = data.time - 1
-        end
-    end
-end
-
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(1000)
-        UpdateTimeouts()
-    end
-end)
-
-function sendToDiscord(admin, victim, action, timeoutMinutes, reason)
-    function GetDiscordId(playerId)
-        local identifiers = GetPlayerIdentifiers(playerId)
-        for _, identifier in ipairs(identifiers) do
-            if string.find(identifier, "discord:") then
-                return string.sub(identifier, 9)
-            end
-        end
-        return nil
-    end
-    
-    function GetFiveMId(playerId)
-        local identifiers = GetPlayerIdentifiers(playerId)
-        for _, identifier in ipairs(identifiers) do
-            if string.find(identifier, "license:") then
-                return string.sub(identifier, 9)
-            end
-        end
-        return nil
-    end
-    local admin_did = GetDiscordId(admin) or "Not Found"
-    local admin_fivemID = GetFiveMId(admin) or "Not Found"
-    local victim_discordID = GetDiscordId(victim) or "Not Found"
-    local victime_FiveMID = GetFiveMId(victim) or "Not Found"
-
-
-
-    local playerName_Victim = GetPlayerName(victim) or "Not Found"
-    local color = (action == "timeout") and 65280 or 16711680 
-
-    local embed = {
-        title = Config.chat_time.discord.title,
-        description = "**Action:** " .. action:sub(1,1):upper() .. action:sub(2) .. "\n",
-        fields = {
-            {name = "Admin", value = admin, inline = true},
-            {name = "Admin Discord ", value = "<@"..admin_did..">", inline = true},
-            {name = "Admin License ", value = admin_fivemID, inline = true},
-            {name = "Victim", value = playerName_Victim, inline = true},
-            {name = "Victim Discord ", value = "<@"..victim_discordID..">", inline = true},
-            {name = "Victim License ", value = victime_FiveMID, inline = true},
-            {name = "Duration", value = timeoutMinutes .. " minutes", inline = true},
-            {name = "Reason", value = reason}
-        },
-        footer = {
-            text = "Date: " .. os.date("%Y-%m-%d %H:%M:%S") .. " | " .. Config.chat_time.discord.footer,
-        },
-        color = color
-    }
-
-    PerformHttpRequest(Config.chat_time.discord.webhook, function(err, text, headers) end, 'POST', json.encode({
-        embeds = { embed }
-    }), {["Content-Type"] = "application/json"})
-end
+-- COMMANDS
 
 RegisterCommand('timeout', function (source, args)
     local playerId = tonumber(args[1])
@@ -150,7 +74,7 @@ RegisterCommand('untimeout', function (source, args)
     end
 end, false)
 
-
+-- EVENTS
 
 AddEventHandler('chatMessage', function(source, name, message)
     if IsPlayerTimedOut(source) and not IsPlayerAceAllowed(source, Config.chat_time.bypass) then
@@ -159,3 +83,76 @@ AddEventHandler('chatMessage', function(source, name, message)
     end
 end)
 
+-- FUNCTIONS
+
+function sendToDiscord(admin, victim, action, timeoutMinutes, reason)
+    local admin_did = GetDiscordId(admin) or "Not Found"
+    local admin_fivemID = GetFiveMId(admin) or "Not Found"
+    local victim_discordID = GetDiscordId(victim) or "Not Found"
+    local victime_FiveMID = GetFiveMId(victim) or "Not Found"
+
+
+
+    local playerName_Victim = GetPlayerName(victim) or "Not Found"
+    local color = (action == "timeout") and 65280 or 16711680 
+
+    local embed = {
+        title = Config.chat_time.discord.title,
+        description = "**Action:** " .. action:sub(1,1):upper() .. action:sub(2) .. "\n",
+        fields = {
+            {name = "Admin", value = admin, inline = true},
+            {name = "Admin Discord ", value = "<@"..admin_did..">", inline = true},
+            {name = "Admin License ", value = admin_fivemID, inline = true},
+            {name = "Victim", value = playerName_Victim, inline = true},
+            {name = "Victim Discord ", value = "<@"..victim_discordID..">", inline = true},
+            {name = "Victim License ", value = victime_FiveMID, inline = true},
+            {name = "Duration", value = timeoutMinutes .. " minutes", inline = true},
+            {name = "Reason", value = reason}
+        },
+        footer = {
+            text = "Date: " .. os.date("%Y-%m-%d %H:%M:%S") .. " | " .. Config.chat_time.discord.footer,
+        },
+        color = color
+    }
+
+    PerformHttpRequest(Config.chat_time.discord.webhook, function(err, text, headers) end, 'POST', json.encode({
+        embeds = { embed }
+    }), {["Content-Type"] = "application/json"})
+end
+
+function TimeoutPlayer(playerId, timeoutMinutes, reason)
+    local timeoutSeconds = timeoutMinutes * 60
+    timeoutList[playerId] = { time = os.time() + timeoutSeconds, reason = reason }
+end
+
+function IsPlayerTimedOut(playerId)
+    return timeoutList[playerId] and os.time() > timeoutList[playerId].time
+end
+
+function UpdateTimeouts()
+    for playerId, data in pairs(timeoutList) do
+        if data.time > 0 then
+            data.time = data.time - 1
+        end
+    end
+end
+
+function GetDiscordId(playerId)
+    local identifiers = GetPlayerIdentifiers(playerId)
+    for _, identifier in ipairs(identifiers) do
+        if string.find(identifier, "discord:") then
+            return string.sub(identifier, 9)
+        end
+    end
+    return nil
+end
+
+function GetFiveMId(playerId)
+    local identifiers = GetPlayerIdentifiers(playerId)
+    for _, identifier in ipairs(identifiers) do
+        if string.find(identifier, "license:") then
+            return string.sub(identifier, 9)
+        end
+    end
+    return nil
+end
